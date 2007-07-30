cc0d76c1722a747340641440fe8fb5ade35a4537
Dave Lambert <d.j.lambert@gmail.com>
2007-07-30 19:45:34 +0100

    Use pushnew to put things on *features*.

--- src/defpackage.lisp
+++ src/defpackage.lisp
@@ -9,5 +9,5 @@
 	   with-ontology))
 
 (eval-when (:load-toplevel)
- (push :ocml cl:*features*)
- (push :ocml-7.5 cl:*features*))
+ (pushnew :ocml cl:*features*)
+ (pushnew :ocml-7.5 cl:*features*))
