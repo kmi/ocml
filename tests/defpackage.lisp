b61f75c39390c6116780498313bd91bb4bd56e0e
Dave Lambert <d.j.lambert@gmail.com>
2007-09-24 01:33:22 +0100

    Move executable code out of tests/defpackage into tests/setup.

--- tests/defpackage.lisp
+++ tests/defpackage.lisp
@@ -1,13 +1,4 @@
 (defpackage :ocml.tests
-  (:nicknames :tests) 
+  (:nicknames :tests)
   (:use :common-lisp :ocml :5am)
   (:export run-all-tests))
-
-(in-package :ocml.tests)
-
-(defun run-all-tests (&optional (break nil))
-  (let ((5am:*debug-on-error* break)
-	(5am:*debug-on-failure* break))
-    (dolist (suite '(concepts-suite namespaces-suite apples-suite owl-suite))
-      (format t "Running ~A: ~%" (symbol-name suite))
-      (run! suite))))
--- tests/setup.lisp
+++ tests/setup.lisp
@@ -3,3 +3,10 @@
 (eval-when (:compile-toplevel :execute :load-toplevel)
   (ocml:initialize-ocml)
   (load "ocml:library;applications;apple-heuristic-classify-redux;load"))
+
+(defun run-all-tests (&optional (break nil))
+  (let ((5am:*debug-on-error* break)
+	(5am:*debug-on-failure* break))
+    (dolist (suite '(concepts-suite namespaces-suite apples-suite owl-suite))
+      (format t "Running ~A: " (symbol-name suite))
+      (run! suite))))
