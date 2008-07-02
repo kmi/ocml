kmi/ocml

commit 768dcd24486c3707371ac95bb44f57ce9da76054

load-ontology-files renamed and rewritten.
Used to be called load-ontology-files-new, which was:

1. A clunky hangover from the dark ages before version control.

2. Confusing, because it gave the impression that this was for loading
   files into a new ontology, not a redefined one.  It's used for
   both.

Rewritten to use with-ontology to ensure error-safe return to any
earlier selected ontology.  finalize-ontology now calls
switch-to-ontology explicitly, because it no longer happens implicitly
through load-ontology-files-new not unselecting the ontology it's
loading (!).

src/theories.lisp
@@ -428,11 +428,11 @@ changed by john domingue 6/2/03
   #+(or :allegro :lispworks) (ocml-record-source-file name 'def-ontology name)
   (when new?
     (add-ontology name ontology))
-  (load-ontology-files-new  ontology files pathname select-this-ontology?)
+  (load-ontology-files ontology files pathname)
   (finalise-ontology ontology)
-  ;;;;;;(maybe-set-rdf-related-information name ontology)
+  (when select-this-ontology?
+    (switch-to-ontology ontology))
   ontology)
-
 
 ;(defun maybe-set-rdf-related-information (name ontology)
 ;  (let ((rdf-namespace-label (ontology-rdf-namespace-label ontology)))
@@ -619,31 +619,14 @@ a definition for ~A ~S  already exists....keeping old definition, inherited from
   ;;;;;(make-ontology-pathname name type)))
   (ocml-load load-file))
 
-;;(defun load-ontology-files (name type files &optional (dir (string name)) (library "LIBRARY"))
-;;  (Loop with dir = (make-ontology-folder dir type library)
-;;	for file in files
-;;	do
-;;	(ocml-load (make-pathname :directory (pathname-directory dir)
-;;                                  :type *lisp-suffix*
-;;                                  :name file))))
-
-
-;;;Redefined - Enrico 25/10/00 - to handle constraint checking properly
-(defun load-ontology-files-new (ontology files pathname select-this-ontology?)
-  (let ((current-ontology *current-ontology*))
-    (switch-to-ontology ontology)
-    (let ((*pending-constraints*))
-      (unwind-protect 
-        (Loop for file in files
-	   do (ocml-load
-	       (merge-pathnames pathname (string-append
-					  file "." *lisp-suffix*)))
-          finally
-          (loop for c in *pending-constraints*
-                do
-                (check-pending-constraints c)))
-        (unless select-this-ontology?
-          (switch-to-ontology current-ontology))))))
+(defun load-ontology-files (ontology files pathname)
+  (with-ontology (ontology)
+    (let ((*pending-constraints* '()))
+      (dolist (file files)
+        (ocml-load (merge-pathnames pathname
+                                    (string-append file "." *lisp-suffix*))))
+      (dolist (c *pending-constraints*)
+        (check-pending-constraints c)))))
 
 
 ;;;CHECK-PENDING-CONSTRAINTS -  Checks the constraint pending from the ontology loading time

commit f5bf3b5149e0b8c041804ddfeae43d1711521813

Fix the redefinition of ontologies.
This was broken in commit 768dcd24486c3707371ac95bb44f57ce9da76054.  A
well intentioned use of with-ontology meant that mutation of the OCML
special variables (the ones that get bound with
with-ocml-thread-safety) happened to the wrong variables: those still
associated with the old copy of ontology-directory.  When an ontology
is redefined, the ocml-ontology object retains its identity, but its
ontology-directory is swapped for an entirely new one.  We call
switch-to-ontology explicitly to bring the new ontology-directory into
play.

src/basic.lisp
@@ -1357,9 +1357,8 @@ relevant slot values"
 ;; interactive entry of an ontology cannot be done, because certain
 ;; operations (such as recomputing slot type specifiers) can be done
 ;; only after the ontology's contents have been seen.
-(defun finalise-ontology (ontology)
-  (with-ontology (ontology)
-    (compute-slot-type-specifiers *current-ontology*)))
+(defun finalise-ontology ()
+  (compute-slot-type-specifiers *current-ontology*))
 
 (defun compute-slot-type-specifiers (ontology)
   "Once the ontology is loaded, compute slot types from the slot type specifiers."

src/theories.lisp
@@ -437,10 +437,15 @@ changed by john domingue 6/2/03
   #+(or :allegro :lispworks) (ocml-record-source-file name 'def-ontology name)
   (when new?
     (add-ontology name ontology))
-  (load-ontology-files ontology files pathname)
-  (finalise-ontology ontology)
+  (with-ontology (ontology)
+    ;; Because the ontology-directory of ONTOLOGY has been set to a
+    ;; new one, we must call SWITCH-TO-ONTOLOGY to force the special
+    ;; variables to be rebound to the new values.
+    (switch-to-ontology ontology)
+    (load-ontology-files files pathname)
+    (finalise-ontology))
   (when select-this-ontology?
-    (switch-to-ontology ontology))
+    (select-ontology name))
   ontology)
 
 ;(defun maybe-set-rdf-related-information (name ontology)
@@ -628,15 +633,13 @@ a definition for ~A ~S  already exists....keeping old definition, inherited from
   ;;;;;(make-ontology-pathname name type)))
   (ocml-load load-file))
 
-(defun load-ontology-files (ontology files pathname)
-  (with-ontology (ontology)
-    (let ((*pending-constraints* '()))
-      (dolist (file files)
-        (ocml-load (merge-pathnames pathname
-                                    (string-append file "." *lisp-suffix*))))
-      (dolist (c *pending-constraints*)
-        (check-pending-constraints c)))))
-
+(defun load-ontology-files (files pathname)
+  (let ((*pending-constraints* '()))
+    (dolist (file files)
+      (ocml-load (merge-pathnames pathname
+                                  (string-append file "." *lisp-suffix*))))
+    (dolist (c *pending-constraints*)
+      (check-pending-constraints c))))
 
 ;;;CHECK-PENDING-CONSTRAINTS -  Checks the constraint pending from the ontology loading time
 ;;;That is, it delays checking instance constraints to avoid unnecessary warning - e.g.,